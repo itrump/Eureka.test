8d416b1b1fb36d53ae495163166f60cfe302959b
itrump <ye515430@gmail.com>
2018-06-02 17:45:42 +0800

    lint

--- Eureka.test/ViewController.swift
+++ Eureka.test/ViewController.swift
@@ -6,23 +6,6 @@
 //  Copyright © 2018 itrump. All rights reserved.
 //
 
-/*import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}*/
-
 import UIKit
 import Eureka
 
